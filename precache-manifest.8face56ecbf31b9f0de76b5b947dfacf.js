6f8cb9d994329e58bcbc2ef6de03f931e6d3b770
Mohammad Hosein <epakchian@gmail.com>
2021-02-05 20:05:01 +0330

    start

---
+++ precache-manifest.8face56ecbf31b9f0de76b5b947dfacf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac7a657c493e96023bb7e7c71cd2f3d",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "14b26d4c61a336255bf0",
+    "url": "/myapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e1d007fce780648c203d",
+    "url": "/myapp/static/css/main.44a373c8.chunk.css"
+  },
+  {
+    "revision": "14b26d4c61a336255bf0",
+    "url": "/myapp/static/js/2.d1847c44.chunk.js"
+  },
+  {
+    "revision": "e1d007fce780648c203d",
+    "url": "/myapp/static/js/main.bac7f325.chunk.js"
+  },
+  {
+    "revision": "e934e9fee62e14cf9385",
+    "url": "/myapp/static/js/runtime-main.d9a25b65.js"
+  },
+  {
+    "revision": "8c8d37c6d2e69a409816bdb59ef812d8",
+    "url": "/myapp/static/media/avatar.8c8d37c6.jpg"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/myapp/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/myapp/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/myapp/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/myapp/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/myapp/static/media/git.c31b5069.svg"
+  }
+]);
